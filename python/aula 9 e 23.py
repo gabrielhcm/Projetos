264cae185b1cdd7e5489269be0776c34d8ec1445
Gabriel Henrique <122419125+gabrielhcm@users.noreply.github.com>
2023-01-18 16:03:47 -0300

    Update aula 9 e 23.py

--- python/aula 9 e 23.py
+++ python/aula 9 e 23.py
@@ -1,6 +1,9 @@
 numero = int(input('digite um numero entre 0 e 9999 '))
 
-numeroc = str(numero)
+u = numero // 1 % 10
+d = numero // 10
 
-print('unidade '.format())
+print('você digitou o numero{} , que com os digitos separados são iguais a '.format(numero))
 
+print(' unidade: {}'.format(u))
+print('dezena: {}'.format(d))

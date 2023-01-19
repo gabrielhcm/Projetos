0c10f2a8b434f2cb207d2c419bedd0a1b060cb19
Gabriel Henrique <122419125+gabrielhcm@users.noreply.github.com>
2023-01-19 17:50:52 -0300

    Update aula 12 ex 44.py

--- python/aula 12 ex 44.py
+++ python/aula 12 ex 44.py
@@ -1,9 +1,33 @@
-num1 = int(input('digite o primeiro numero '))
-num2 = int(input('digite o segundo numero '))
+preçoc = float(input(' digite o preço total das compras!'))
 
-if num1 > num2:
- print('O primeiro numero é maior')
-elif num2 > num1:
- print('O segundo numero é maior')
-elif num1 == num2:
- print('Os dois numeros sao iguais')     
+print(''' escolha a forma de pagamento
+         [1] a vista dinheiro ou cheque
+         [2] a vista no cartão
+         [3] em até 2x no cartão
+         [4] 3x ou mais
+
+''')
+escolha = int(input())
+
+preço1 = (preçoc*0.10) 
+preço1final =  preçoc  - preço1
+
+preço2 = (preçoc*0.05) 
+preço2final =  preçoc -  preço2 
+
+preço3 = (preçoc*0.20) + preçoc
+preço3final = preço3   
+
+
+if escolha == 1:
+    print('o custo das suas compras foi: {}'.format(preço1final))
+ 
+elif escolha ==2:
+     print('o custo das suas compras foi: {}'.format(preço2final))
+
+elif escolha ==3:  
+    print('o custo das suas compras foi: {}'.format(preçoc))
+
+elif escolha ==4: 
+
+ print('o custo das suas compras foi: {}'.format(preço3final))

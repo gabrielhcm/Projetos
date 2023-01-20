bd746faffea4ed831c9130cfd6c0dd971ddf479b
Gabriel Henrique <122419125+gabrielhcm@users.noreply.github.com>
2023-01-20 00:30:11 -0300

    a

--- python/aula 13 ex 52.py
+++ python/aula 13 ex 52.py
@@ -1,6 +1,7 @@
 num = int(input('digite um numero'))
-
+total = 0
 for c in range ( 1, num +1):
     if num % c ==0:
-     print('\033[33m',end= ' ')    
+     print('\033[33m',end= ' ')  
+    total += 1
 print('{}'.format(c))

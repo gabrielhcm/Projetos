15bc0ddd78a28357d3372554931424684d156895
Gabriel Henrique <122419125+gabrielhcm@users.noreply.github.com>
2023-01-20 23:56:05 -0300

    beee

---
+++ python/bee 1006.py
@@ -0,0 +1,15 @@
+a = float (input())
+b = float (input())
+c = float (input())
+
+peso = 2 + 3 + 5
+pesoa = a*2
+pesob = b*3
+pesoc = c*5
+
+mediaf = pesoa + pesob + pesoc
+mediafinal = mediaf / peso
+
+
+
+print('MEDIA = {:.1f}'.format(mediafinal))
---
+++ python/bee 1008.py
@@ -0,0 +1,12 @@
+a = int (input())
+b = int (input())
+c = int (input())
+d = int (input())
+
+
+mediafinal = (a*b)- c*d 
+
+
+
+
+print('DIFERENCA = {:.1f}'.format(mediafinal))

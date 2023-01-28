07725f4d3a244f71fbbc0c0bcf51da78b10d1615
Gabriel Henrique <122419125+gabrielhcm@users.noreply.github.com>
2023-01-28 13:16:50 -0300

    :(

---
+++ JavaS/aulas Js/curso em vidio exes/video ex 04/script.js
@@ -0,0 +1,12 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.lenght == 0) {
+        window.alert('por favor, digite um nuemro!')
+    
+    
+    } else{
+        let n = Number(num.value)
+    }
+   
+}
--- JavaS/aulas Js/curso em vidio exes/video ex 3/script.js
+++ JavaS/aulas Js/curso em vidio exes/video ex 3/script.js
@@ -1,7 +1,29 @@
-let batata = 1
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    if (ini.Value.length == 0 || fim.Value.length == 0 || passo.Value.length == 0) {
+        window.alert('erro')
+    }   else {
+        res.innerHTML = 'contando: '
+        let i = number(ini.Value)
+        let f = number(fim.Value)
+        let p = number(passo.Value)
+        if (i<f) {
+        
+            for (let c = i; c <= f;c += p) {
+                res.innerHTML += ` ${(c.Value)}`
+            }
+        }else{
+            for(let c = i;c >=f;c-=p){
+                res.innerHTML += ` ${(c.Value)}`
+            }
+        }
+        res.innerHTML+=`\u`
+    }
 
-while(batata <6){
-    console.log (`${batata} batatas`)
-    batata++
-    //console.log(`numero de batatas ${batata}`)
+
 }
+
+

6852f9beda97752fab322f702fe0442596692ae4
Gabriel Henrique <122419125+gabrielhcm@users.noreply.github.com>
2023-02-02 04:07:22 -0300

    :(

---
+++ JavaS/odin/odin-rock-paper/rockppsiz.js
@@ -0,0 +1,26 @@
+function play(playerselec, pcselec){
+    playerselec = prompt(" digite pedra papel ou tesoura!");
+    pcselec = 0
+    pcrand = Math.random() * 3;
+
+   // if (pcrand === 1) {
+     //   playerselec = "pedra"
+    
+    // } else if  (pcrand=== 2) {
+     //  playerselec = "tesoura" 
+    
+    // } else if (pcrand === 3) {
+     //    playerselec = "pedra"
+    
+    // } else{
+         //"erro"
+    // }
+    
+    
+    //for (let i = 0; i<5; i++) {
+
+    //}
+
+    console.log(pcselec)   
+
+}
